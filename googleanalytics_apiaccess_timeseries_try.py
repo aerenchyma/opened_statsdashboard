235d8feb20f52d03398a6879e1968253925cc267
jczetta <jczetta@gmail.com>
2013-06-14 11:36:08 -0400

    workarounds, adding some stuff for report usefulness, no other files yet committed

--- googleanalytics_apiaccess_timeseries_try.py
+++ googleanalytics_apiaccess_timeseries_try.py
@@ -20,6 +20,11 @@ CLIENT_SECRETS = 'client_secrets.json'
 # helpful msg if it's missing
 MISSING_CLIENT_SECRETS_MSG = '%s is missing' % CLIENT_SECRETS
 
+profileid= "8335957" 
+pgpath = "/education/lsa/physics140/fall2007"
+
+param_list = [profileid, pgpath]
+
 # flow object to be used if we need to authenticate (?)
 FLOW = flow_from_clientsecrets(CLIENT_SECRETS, scope='https://www.googleapis.com/auth/analytics.readonly', message=MISSING_CLIENT_SECRETS_MSG)
 
@@ -59,17 +64,32 @@ def initialize_service():
 # Expects profile ID as first CLI argument
 # Expects path ID as second CLI arg
 
-def main(argv):
+def deal_with_results(res):
+	view_nums = [x[1] for x in res] # y axis
+	date_strs = [mdates.datestr2num(x[0]) for x in res] # hmm these are strings... # x axis
+	#date_strs = [mdates.strpdate2num(x[0],'%m-%d') for x in res]
+	#print type(date_strs[1])
+	plt.plot_date(x=date_strs, y=view_nums, fmt="r-")
+	plt.tick_params(labelsize=8)
+	plt.grid(True)
+	plt.title("Course Views over past %s days" % (len(date_strs)-1)) # should get title of course
+	#plt.autofmt_xdate()
+	savefig('test2.png')
+
+
+#def main(argv):
+def main(paramlist):
 	#proper_start_date()
 	service = initialize_service()
 	try:
-		profile_id = sys.argv[1]
+		profile_id = paramlist[0]#sys.argv[1]
 		if profile_id:
+			#print profile_id
 			#query core reporting api
 			results = get_results(service, profile_id)
 			#print_results(results) # hmm formatting, also where should these go
 			res = return_results(results)
-			print res
+			#print res
 	except TypeError, error:
 		print "There was an API error: %s " % (error)
 	except HttpError, error:
@@ -80,22 +100,23 @@ def main(argv):
 	 	print "Did you provide a profile id and a path as cli arguments? Try again."
 	else: # should run if it did not hit an except clause
 
-
-		view_nums = [x[1] for x in res] # y axis
-		date_strs = [mdates.datestr2num(x[0]) for x in res] # hmm these are strings... # x axis
+		deal_with_results(res)
+		# view_nums = [x[1] for x in res] # y axis
+		# date_strs = [mdates.datestr2num(x[0]) for x in res] # hmm these are strings... # x axis
 		
-		plt.plot_date(x=date_strs, y=view_nums, fmt="r-")
-		plt.tick_params(labelsize=10)
-		plt.grid(True)
-		savefig('test2.png')
+		#return view_nums, date_strs
+		# newplot = plt.plot_date(x=date_strs, y=view_nums, fmt="r-")
+		# plt.tick_params(labelsize=10)
+		# plt.grid(True)
+		# savefig('test2.png')
 		#plt.show()
 
 def get_results(service, profile_id):
 	# query = service.data().ga().get(ids='ga:%s' % profile_id, start_date='2010-03-01',end_date='2013-05-15',metrics='ga:pageviews',dimensions='ga:pagePath',filters='ga:pagePath==%s' % (sys.argv[2]))
-	start = proper_start_date(15)
+	start = proper_start_date(30)
 	end = str(date.today())
 	# return query.execute()
-	return service.data().ga().get(ids='ga:%s' % (profile_id), start_date="%s" % start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (sys.argv[2])).execute()
+	return service.data().ga().get(ids='ga:%s' % (profile_id), start_date="%s" % start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (param_list[1])).execute()#(sys.argv[2])).execute()
 
 
 def return_results(results):
@@ -126,7 +147,8 @@ if __name__ == '__main__':
 	#query = {'ids':'ga:%s' % profile_id, 'start_date':'2010-03-01','end_date':'2013-05-12','metrics'='ga:pageviews','dimensions':'ga:pagePath','filters':'ga:pagePath==%s' % (sys.argv[2]))
 	#query = service.data().ga().get(ids='ga:%s' % profile_id, start_date='2010-03-01',end_date='2013-05-15',metrics='ga:pageviews',dimensions='ga:pagePath',filters='ga:pagePath==%s' % (sys.argv[2]))
 
-	main(sys.argv)
+	#main(sys.argv)
+	main(param_list)
 
 
 
