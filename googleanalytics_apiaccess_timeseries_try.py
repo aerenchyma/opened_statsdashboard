15be06e2db39ce9aa2bb38007a3dcab984c40565
jczetta <jczetta@gmail.com>
2013-06-17 15:02:32 -0400

    subclasses for bulk downloads plot and dls/views plot

--- googleanalytics_apiaccess_timeseries_try.py
+++ googleanalytics_apiaccess_timeseries_try.py
@@ -79,14 +79,14 @@ class GoogleAnalyticsData(object):
 		savefig('test3.png')
 
 	def main(self):
-		service = self.initialize_service()
+		self.service = self.initialize_service()
 		try:
 			self.profile_id = self.paramlist[0]#sys.argv[1]
 			#print "still in main"
 			if self.profile_id:
 				#print profile_id
 				#query core reporting api
-				results = self.get_results(service, self.profile_id)
+				results = self.get_results(self.service, self.profile_id)
 				#print_results(results) # hmm formatting, also where should these go
 				res = self.return_results(results)
 				#print res
@@ -106,7 +106,7 @@ class GoogleAnalyticsData(object):
 		start = self.proper_start_date() # change to change num of days back 
 		end = str(date.today())
 		# return query.execute()
-		return service.data().ga().get(ids='ga:%s' % (profile_id), start_date=start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (self.paramlist[1])).execute()#(sys.argv[2])).execute()
+		return self.service.data().ga().get(ids='ga:%s' % (profile_id), start_date=start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (self.paramlist[1])).execute()#(sys.argv[2])).execute()
 
 
 	def return_results(self, results):
@@ -131,13 +131,14 @@ class GoogleAnalyticsData(object):
 		# for modularity -- poss look at Python print-results examples (e.g. by country or whatever) todo
 
 
-class GABulkDownloads(GoogleAnalyticsData):
+class GABulkDownloads_Views(GoogleAnalyticsData):
 	def __init__(self, days_back=30):
 		self.days_back = days_back
 		self.CLIENT_SECRETS = 'client_secrets.json'
 		# helpful msg if it's missing
 		self.MISSING_CLIENT_SECRETS_MSG = '%s is missing' % self.CLIENT_SECRETS
 		self.paramlist = [int(infofile.profileid),self.get_bulk_dl_link()] # needs error checking TODO
+		self.paramlist_second = [int(infofile.profileid), infofile.pgpath]
 		self.FLOW = flow_from_clientsecrets(self.CLIENT_SECRETS, scope='https://www.googleapis.com/auth/analytics.readonly', message=self.MISSING_CLIENT_SECRETS_MSG)
 		self.TOKEN_FILE_NAME = 'analytics.dat'
 		#print self.paramlist
@@ -155,19 +156,44 @@ class GABulkDownloads(GoogleAnalyticsData):
 				if "Download all" in link.text: # depends on current page lang/phrasing
 					response = br.follow_link(link)
 					url = response.geturl()
-			return url[len("http://open.umich.edu"):]
+			return url[len("http://open.umich.edu"):] # if no Download All, error -- needs checking + graceful handling
 
+	def get_results_other(self, service, profile_id):
+		# query = service.data().ga().get(ids='ga:%s' % profile_id, start_date='2010-03-01',end_date='2013-05-15',metrics='ga:pageviews',dimensions='ga:pagePath',filters='ga:pagePath==%s' % (sys.argv[2]))
+		start = self.proper_start_date() # change to change num of days back 
+		end = str(date.today())
+		# return query.execute()
+		return self.service.data().ga().get(ids='ga:%s' % (profile_id), start_date=start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (self.paramlist_second[1])).execute()#(sys.argv[2])).execute()
+
+
+	def deal_with_results(self, res):
+		"""Handles results gotten from API and formatted, plots them with matplotlib tools and saves plot img"""
+		view_nums = [x[1] for x in res] # y axis
+		view_nums_orig = [x[1] for x in self.return_results(self.get_results_other(self.service,self.profile_id))] ## let's see
+		date_strs = [mdates.datestr2num(x[0]) for x in res] # x axis
+		fig, ax = plt.subplots(1)
+		ax.plot_date(date_strs, view_nums, fmt="b-")
+		ax.plot_date(date_strs, view_nums_orig, fmt="g-")
+		fig.autofmt_xdate()
+		ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+		total = sum(view_nums)
+		plt.title("Course Views vs Bulk Material Downloads over past %s days" % (len(date_strs)-1)) # should get title of course
+		savefig('test4.png')
+
+
+class GABulkDownloads(GABulkDownloads_Views):
 	def deal_with_results(self, res):
 		"""Handles results gotten from API and formatted, plots them with matplotlib tools and saves plot img"""
 		view_nums = [x[1] for x in res] # y axis
 		date_strs = [mdates.datestr2num(x[0]) for x in res] # x axis
 		fig, ax = plt.subplots(1)
-		ax.plot_date(date_strs, view_nums, fmt="b-")
+		ax.plot_date(date_strs, view_nums, fmt="r-")
 		fig.autofmt_xdate()
 		ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
 		total = sum(view_nums)
 		plt.title("%d total Bulk Course Material Downloads over past %s days" % (total, len(date_strs)-1)) # should get title of course
-		savefig('test4.png')
+		savefig('test5.png')
+
 
 
 if __name__ == '__main__':
@@ -179,8 +205,11 @@ if __name__ == '__main__':
 	#print a.paramlist[0]
 	a.main()
 
+	c = GABulkDownloads_Views()
+	c.main()
+
 	b = GABulkDownloads()
-	print b.paramlist
+	#print b.paramlist
 	b.main()
 
 
--- make_plots_overall.py
+++ make_plots_overall.py
@@ -6,8 +6,14 @@ def main():
 	# course views over time (input eventually for days previous + path to investigate [latter for all, infofile])
 	nd = gatt.GoogleAnalyticsData()
 	nd.main()
+	# course bulk downloads over time
+	nbdls = gatt.GABulkDownloads()
+	nbdls.main()
+	# plot of course bulk downloads w/ course views
+	dlsv = gatt.GABulkDownloads_Views()
+	dlsv.main()
 
-	# other .. ADD; TODO
+	# next == plot on same plot with legend ?
 
 if __name__ == '__main__':
 	main()
