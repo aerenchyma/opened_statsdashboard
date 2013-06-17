f8ee08b47858c5044d6c613e8a628ea0c642ee11
jczetta <jczetta@gmail.com>
2013-06-17 11:30:46 -0400

    makeplotsoverall works for OO structure

--- make_plots_overall.py
+++ make_plots_overall.py
@@ -1,18 +1,13 @@
 import googleanalytics_apiaccess_timeseries_try as gatt 
 import infofile
 
-profileid = infofile.profileid
-pgpath = infofile.pgpath
-param_list = [profileid,pgpath]
-
-
-
-
 
 def main():
 	# course views over time (input eventually for days previous + path to investigate [latter for all, infofile])
-	gatt.main(param_list)
+	nd = gatt.GoogleAnalyticsData()
+	nd.main()
 
+	# other .. ADD; TODO
 
 if __name__ == '__main__':
 	main()
