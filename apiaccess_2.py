d7cb4fd64cfa881710098112c26806cbf78f1ec3
jczetta <jczetta@gmail.com>
2013-06-27 14:52:10 -0400

    Discovered bug in new page writing, fixed in messy way, todo cleanup

---
+++ apiaccess_2.py
@@ -0,0 +1,216 @@
+import sys
+import infofile
+from pylab import * #?
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+from datetime import date, timedelta
+from apiclient.errors import HttpError
+from oauth2client.client import AccessTokenRefreshError
+import httplib2
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+
+# structural stuff, TODO
+# generalization; TODO
+
+class GoogleAnalyticsData(object):
+	def __init__(self, days_back=30):
+		self.days_back = days_back
+		self.CLIENT_SECRETS = 'client_secrets.json'
+		# helpful msg if it's missing
+		self.MISSING_CLIENT_SECRETS_MSG = '%s is missing' % self.CLIENT_SECRETS
+		self.paramlist = [int(infofile.profileid),infofile.pgpath] # should this be here or in overall file??
+		# flow object to be used if we need to authenticate (this remains a bit of a problem in some cases)
+		self.FLOW = flow_from_clientsecrets(self.CLIENT_SECRETS, scope='https://www.googleapis.com/auth/analytics.readonly', message=self.MISSING_CLIENT_SECRETS_MSG)
+
+		# a file to store the access token
+		self.TOKEN_FILE_NAME = 'analytics.dat' # should be stored in a SECURE PLACE
+
+
+	def proper_start_date(self):
+		"""Gets accurate date in YYYY-mm-dd format that is 30 days earlier than current day"""
+		d = date.today() - timedelta(days=self.days_back)
+		return str(d)
+
+
+	def prepare_credentials(self):
+		# get existing creds
+		storage = Storage(self.TOKEN_FILE_NAME)
+		credentials = storage.get()
+
+		# if existing creds are invalid and Run Auth flow
+		# run method will store any new creds
+		if credentials is None or credentials.invalid:
+			credentials = run(FLOW, storage)
+		return credentials
+
+	def initialize_service(self):
+		http = httplib2.Http()
+		credentials = self.prepare_credentials()
+		http = credentials.authorize(http) # authorize the http obj
+		return build('analytics', 'v3', http=http)
+
+
+	def deal_with_results(self,res):
+		"""Handles results gotten from API and formatted, plots them with matplotlib tools and saves plot img"""
+		view_nums = [x[1] for x in res] # y axis
+		date_strs = [mdates.datestr2num(x[0]) for x in res]
+		fig, ax = plt.subplots(1)
+		ax.plot_date(date_strs, view_nums, fmt="g-")
+		fig.autofmt_xdate()
+		ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+		total = sum(view_nums)
+		plt.title("%d total Course Views over past %s days" % (total, len(date_strs)-1)) # should get title of course
+		plt.text(3,3,"TESTING ADDING A STRING THING TO PLOT PDF")
+		return fig
+
+	def main(self):
+		self.service = self.initialize_service()
+		try:
+			self.profile_id = self.paramlist[0]
+			if self.profile_id:
+				results = self.get_results(self.service, self.profile_id)
+				res = self.return_results(results)
+		except TypeError, error:
+			print "There was an API error: %s " % (error)
+		except HttpError, error:
+			print "There was an API error: %s " % (error)
+		except AccessTokenRefreshError:
+			print "The credentials have been revoked or expired, please re-run app to reauthorize."
+		except:
+		 	print "Did you provide a profile id and a path as cli arguments? Try again."
+		else: # should run if it did not hit an except clause
+			return self.deal_with_results(res)
+
+	def get_results(self, service, profile_id):
+		# query = service.data().ga().get(ids='ga:%s' % profile_id, start_date='2010-03-01',end_date='2013-05-15',metrics='ga:pageviews',dimensions='ga:pagePath',filters='ga:pagePath==%s' % (sys.argv[2]))
+		start = self.proper_start_date() # change to change num of days back 
+		end = str(date.today())
+		# return query.execute()
+		return self.service.data().ga().get(ids='ga:%s' % (profile_id), start_date=start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (self.paramlist[1])).execute()#(sys.argv[2])).execute()
+
+
+	def return_results(self, results):
+		if results:
+			#date_views_tup = [(str(x[0][-4:-2])+"/"+str(x[0][-2:]),int(x[1])) for x in results.get('rows')] ## altered date strs
+				# should be list of tuples of form: ("mm/dd", views) where views is int
+			date_views_tup = [(str(x[0]), int(x[1])) for x in results.get('rows')]
+			return date_views_tup
+		else:
+			print "No results found."
+			return None
+
+	def print_results(self, results):
+		# print data nicely for the user (may also want to pipe to a file)
+		## this turned into a testing fxn -- TODO decide whether/what printing is needed and change to class __str__ method
+		if results:
+			print "Profile: %s" % results.get('profileInfo').get('profileName')
+			#print 'Total Pageviews: %s' % results.get('rows')[0][1]
+			for r in results.get('rows'):
+				print r
+		else:
+			print "No results found."
+		# for modularity -- poss look @ Python print-results examples (e.g. by country or whatever) todo
+
+
+class GABulkDownloads_Views(GoogleAnalyticsData):
+	def __init__(self, days_back=30):
+		self.days_back = days_back
+		self.CLIENT_SECRETS = 'client_secrets.json'
+		# helpful msg if it's missing
+		self.MISSING_CLIENT_SECRETS_MSG = '%s is missing' % self.CLIENT_SECRETS
+		self.paramlist = [int(infofile.profileid),self.get_bulk_dl_link()] # needs error checking TODO
+		self.paramlist_second = [int(infofile.profileid), infofile.pgpath]
+		self.FLOW = flow_from_clientsecrets(self.CLIENT_SECRETS, scope='https://www.googleapis.com/auth/analytics.readonly', message=self.MISSING_CLIENT_SECRETS_MSG)
+		self.TOKEN_FILE_NAME = 'analytics.dat'
+
+	def get_bulk_dl_link(self):
+		try:
+			import mechanize
+			br = mechanize.Browser()
+		except:
+			print "Dependency (Mechanize) not installed. Try again."
+			return None
+		else:
+			response = br.open("http://open.umich.edu%s" % infofile.pgpath)
+			for link in br.links():
+				if "Download all" in link.text: # depends on current page lang/phrasing
+					response = br.follow_link(link)
+					url = response.geturl()
+			return url[len("http://open.umich.edu"):] # if no Download All, error -- needs checking + graceful handling
+
+	def get_results_other(self, service, profile_id):
+		# query = service.data().ga().get(ids='ga:%s' % profile_id, start_date='2010-03-01',end_date='2013-05-15',metrics='ga:pageviews',dimensions='ga:pagePath',filters='ga:pagePath==%s' % (sys.argv[2]))
+		start = self.proper_start_date() # change to change num of days back 
+		end = str(date.today())
+		# return query.execute()
+		return self.service.data().ga().get(ids='ga:%s' % (profile_id), start_date=start,end_date=end,metrics='ga:pageviews',dimensions='ga:date',sort='ga:date',filters='ga:pagePath==%s' % (self.paramlist_second[1])).execute()#(sys.argv[2])).execute()
+
+
+	def deal_with_results(self, res):
+		"""Handles results gotten from API and formatted, plots them with matplotlib tools and saves plot img"""
+		view_nums = [x[1] for x in res] # y axis
+		view_nums_orig = [x[1] for x in self.return_results(self.get_results_other(self.service,self.profile_id))] ## let's see
+		date_strs = [mdates.datestr2num(x[0]) for x in res] # x axis
+		fig, ax = plt.subplots(1)
+		ax.plot_date(date_strs, view_nums, fmt="b-", label="Downloads")
+		ax.plot_date(date_strs, view_nums_orig, fmt="g-", label="Views")
+		fig.autofmt_xdate()
+		ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+		#total = sum(view_nums)
+		plt.legend(loc='upper left')
+		plt.title("Course Views vs Bulk Material Downloads over past %s days" % (len(date_strs)-1)) # should get title of course
+		#savefig('test4.png')
+		return fig
+
+
+class GABulkDownloads(GABulkDownloads_Views):
+	def deal_with_results(self, res):
+		"""Handles results gotten from API and formatted, plots them with matplotlib tools and saves plot img"""
+		view_nums = [x[1] for x in res] # y axis
+		date_strs = [mdates.datestr2num(x[0]) for x in res] # x axis
+		fig, ax = plt.subplots(1)
+		ax.plot_date(date_strs, view_nums, fmt="b-")
+		fig.autofmt_xdate()
+		ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+		total = sum(view_nums)
+		plt.title("%d total Bulk Course Material Downloads over past %s days" % (total, len(date_strs)-1)) # should get title of course
+		#savefig('test5.png')
+		#fig.show()
+		return fig
+
+class GA_Text_Info(GABulkDownloads_Views):
+	def deal_with_results(self, res):
+		"""Handles results gotten from API and formatted, returns data"""
+		view_nums = [x[1] for x in res] # y axis
+		view_nums_orig = [x[1] for x in self.return_results(self.get_results_other(self.service,self.profile_id))] ## let's see
+		total_dls = sum(view_nums)
+		total_views = sum(view_nums_orig)
+		# get more info with other queries? TODO
+		info_dict = {'Across time span':self.days_back, 'Total Page Views': total_views, 'Total Bulk Downloads': total_dls}
+		return info_dict
+
+
+
+if __name__ == '__main__':
+	## TESTING (pre unit tests)
+	#main(sys.argv)
+	#main(param_list)
+	#print "running the right file"
+	a = GoogleAnalyticsData()
+	#print a.paramlist[0]
+	a.main()
+
+	c = GABulkDownloads_Views()
+	c.main()
+
+	b = GABulkDownloads()
+	#print b.paramlist
+	b.main()
+
+
+
+
---
+++ make_plots_overall_2.py
@@ -0,0 +1,156 @@
+import apiaccess_2 as gatt 
+import infofile
+from matplotlib.backends.backend_pdf import PdfPages
+from fpdf import FPDF
+from pyPdf import PdfFileReader, PdfFileWriter
+import os.path
+from pudb import set_trace
+
+PAGEVIEWS_DEFN = """
+The phrase PAGE VIEWS here refers to the number of times a page has been loaded. 
+If you refresh the page three times, that is 3 page views.
+"""
+
+VISITS_DEFN = """
+The phrase VISITS here refers to the number of times a page has been visited at all.
+This means that if you load a page on Tuesday, don't refresh it, close your computer,
+then open the computer on Wednesday with the page still open, and do not refresh it then
+either, this all counts as 1 view. It counts as 1, even if 
+"""
+
+
+## create infosheet part of pdf
+# pdf = FPDF()
+# pdf.add_page()
+# pdf.set_font('Arial','',12)
+# pdf.cell(40,10, "hello world test!") # here's where info goes on a page
+# pdf.ln() # line break
+# pdf.cell(40,10, "second test line") # here too
+# pdf.output('newtry1.pdf', 'F')
+
+## this was all a test for understanding what bits add pages, and this worked
+# sec_pdf = FPDF()
+# sec_pdf.add_page()
+# sec_pdf.set_font('Arial','',12)
+# sec_pdf.cell(40,10, "YET ANOTHER TEST")
+# sec_pdf.output('sectry1.pdf','F')
+#sec_fh = file("sectry1.pdf", "rb")
+
+# output = PdfFileWriter()
+
+
+
+
+# fname = "summary3.pdf" # file to open and start w/ -- dynamically though, created below
+# # so this Real Code should go below its creation in the main fxn
+# fh = file(fname, "rb")
+# inp = PdfFileReader(fh)
+
+# #print inp.getDocumentInfo().title
+
+# for i in range(inp.getNumPages()):
+# 	output.addPage(inp.getPage(i))
+
+# newfile = "newtry1.pdf" # again, la TODO -- this should always be the same b/c tmp file
+# fh_new = file(newfile, "rb")
+# newf = PdfFileReader(fh_new)
+# #sec_newf = PdfFileReader(sec_fh)
+# output.addPage(newf.getPage(0))
+# #output.addPage(sec_newf.getPage(0))
+
+# # #output.addPage(inp.getPage(0))
+
+# # #output.addPage(inp.getPage(1).rotateClockwise(90))
+
+# # # for p in inp.pages():
+# # # 	output.addPage(p)
+
+
+
+# # # print "doc 1 has %s pages" % (inp.getNumPages())
+# # # print "doc 2 has %s pages" % (output.getNumPages())
+
+# outpStream = file("sec_summary_new1_output2.pdf", "wb") # and this should overwrite the summary, when generated
+# output.write(outpStream)
+# outpStream.close()
+
+def text_page_pdf(info_dict,fname, origfile, tmpfile="tmp_file_num.pdf"): # fname is the summary filename -- perhaps rename var TODO
+	pdf = FPDF()
+	#set_trace()
+	pdf.add_page()
+	pdf.set_font('Arial','',12) # adjust as appropriate
+	x,y = 40,10
+	for k in info_dict:
+		if k != "Across time span":
+			pdf.cell(40,10, "%s: %d" % (k,info_dict[k]))
+		pdf.ln()
+	pdf.ln()
+	# y += 10
+	pdf.cell(x,y, "across time span of %s days" % (info_dict["Across time span"]))
+	pdf.output(tmpfile, 'F')
+
+	output = PdfFileWriter()
+	fname = fname
+	inp = PdfFileReader(file(origfile, "rb"))
+	for i in range(inp.getNumPages()):
+		output.addPage(inp.getPage(i))
+	newf = PdfFileReader(file(tmpfile, "rb"))
+	output.addPage(newf.getPage(0))
+	outpStream = file(fname, "wb")
+	output.write(outpStream)
+	outpStream.close()
+
+
+
+def main():
+	# course views over time (input eventually for days previous + path to investigate [latter for all, infofile])
+	#nd, nbdls, dlsv = 
+	objs_for_plots = gatt.GoogleAnalyticsData(100), gatt.GABulkDownloads(100), gatt.GABulkDownloads_Views(100)
+	plots = [x.main() for x in objs_for_plots]
+	pp = PdfPages('oo_summary_1.pdf')
+	throwaway = [pp.savefig(x) for x in plots]
+	pp.close()
+
+	# adding page with info ## -- should this be abstracted more? TODO
+	info_obj = gatt.GA_Text_Info()
+	info = info_obj.main() # returns infodict
+	text_page_pdf(info, "oo_summary_4.pdf", "oo_summary_1.pdf") ## throwing error -- test with new name and we'll see
+
+	## create infosheet part of pdf
+	# pdf = FPDF()
+	# pdf.add_page()
+	# pdf.set_font('Arial','',12)
+	# pdf.cell(40,10, "TESTING NEW PAGE OVERWRITE") # here's where info goes on a page
+	# pdf.ln() # line break
+	# pdf.cell(40,10, "second test line!!") # here too
+	# pdf.output('newtry1.pdf', 'F')
+
+	# output = PdfFileWriter()
+
+	# fname = "oo_summary_1.pdf" # whatever file created name, this must be the same
+	# fh = file(fname, "rb")
+	# inp = PdfFileReader(fh)
+	# for i in range(inp.getNumPages()):
+	# 	output.addPage(inp.getPage(i))
+
+	# newfile = "newtry1.pdf" # again, la TODO -- this should always be the same as one created above after FPDF() constr
+	# fh_new = file(newfile, "rb")
+	# newf = PdfFileReader(fh_new)
+	# output.addPage(newf.getPage(0))
+
+	# outpStream = file("oo_summary_3.pdf", "wb") # and this should overwrite the summary, when generated -- testing actual overwrite now
+	# ## appears that overwrite of this file is the problem?? why? is it a hash issue? what expects a base10 int that changes w/ that?
+	# ## that works, though
+	# output.write(outpStream)
+	# outpStream.close()
+
+
+	for k in info:
+		print k, info[k]
+
+# def save_pdf():
+# 	return None
+
+if __name__ == '__main__':
+	main()
+
